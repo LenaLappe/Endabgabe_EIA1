0c8edd93c383456ea9c35e74db76f4bb1db0ae36
LenaLappe <80837514+LenaLappe@users.noreply.github.com>
2021-06-16 23:33:06 +0200

    icons doesn't work

--- ts/typescript_easy.js
+++ ts/typescript_easy.js
@@ -3,49 +3,69 @@ window.addEventListener("load", function () {
     var levelButtons = document.querySelector(".levelButtons");
     var easyButton = document.getElementById("easyButton");
     var containerEasy = document.querySelector(".containerEasy");
-    var players = ["x", "o"];
-    var currentPlayer = true;
-    var state = "";
-    var index = 0;
+    var currentPlayerIsPlayer0 = true;
     var board = 8;
-    var field = "";
     var gameboard;
-    var myArray = ["free", "free", "free", "free", "free", "free", "free", "free", "free"];
+    var myArray = [{ symbol: "free" }, { symbol: "free" }, { symbol: "free" }, { symbol: "free" }, { symbol: "free" }, { symbol: "free" }, { symbol: "free" }, { symbol: "free" }, { symbol: "free" }];
     easyButton.addEventListener("click", drawField);
-    easyButton.addEventListener("click", function () {
-        //alle Buttons verschwinden lassen
+    function drawField() {
+        //alle Buttons verschwinden lassen und HTML einmal grundleeren
         levelButtons.innerHTML = "";
-    });
-    function drawField(event) {
-        //leichtes Spielfeld mit 9 divs erstellen
-        for (index = 0; index <= board; index++) {
-            myArray[index];
+        //nach jedem durchlauf Container leeren, sonst generieren sich immer neue 9 divs
+        containerEasy.innerHTML = "";
+        var _loop_1 = function (i) {
             gameboard = document.createElement("div");
             gameboard.classList.add("field");
             var idField = document.createAttribute("id");
-            idField.value = index.toString();
+            idField.value = i.toString();
             containerEasy.appendChild(gameboard);
-            console.log(index);
-        }
-        //Felder werden mit ensprechenden icons gefüllt
-        if (players[0]) {
-            var turn = document.createElement("i");
-            turn.classList.add("fas fa-times");
-            console.log("Kreuz");
-        }
-        else {
-            var otherTurn = document.createElement("i");
-            otherTurn.classList.add("far fa-circle");
-        }
-        //überwachung der freien Felder, ob gedrückt wurde
-        if (myArray.state == "free") {
-            document.querySelector(".field").addEventListener("click", function () { clickFunction(); });
+            gameboard.setAttributeNode(idField);
+            console.log(i);
+            //Felder werden mit ensprechenden icons gefüllt
+            //Spieler1 = x
+            if (myArray[i].symbol == "x") {
+                var turn = document.createElement("i");
+                turn.classList.add("fas", "fa-times");
+                turn.style.color = "white";
+                turn.style.fontFamily = "cryon";
+                console.log("Kreuz");
+                gameboard.appendChild(turn);
+            }
+            //Spieler2 = o
+            else if (myArray[i].symbol == "o") {
+                var otherTurn = document.createElement("i");
+                otherTurn.classList.add("far", "fa-circle");
+                otherTurn.style.color = "white";
+                otherTurn.style.fontFamily = "cryon";
+                console.log("Kreis");
+                gameboard.appendChild(otherTurn);
+            }
+            //überwachung der freien Felder, ob gedrückt wurde
+            else if (myArray[i].symbol == "free") {
+                console.log("i is " + i);
+                gameboard.addEventListener("click", function () { clickFunction(i); });
+            }
+        };
+        //leichtes Spielfeld mit 9 divs erstellen
+        for (var i = 0; i <= board; i++) {
+            _loop_1(i);
         }
     }
-    function clickFunction() {
-        if (currentPlayer == true) {
+    function clickFunction(positionImArray) {
+        console.log("position is " + positionImArray);
+        for (var index = 0; index <= board; index++) {
+            if (positionImArray == index) {
+                if (currentPlayerIsPlayer0 == true) {
+                    myArray[index].symbol = "x";
+                    console.log("Kreuz gedrückt");
+                }
+                else {
+                    myArray[index].symbol = "o";
+                    console.log("Kreis gedrückt");
+                }
+            }
         }
-        currentPlayer = !currentPlayer;
+        currentPlayerIsPlayer0 = !currentPlayerIsPlayer0;
         drawField();
     }
     //gewinnen einer Runde
--- ts/typescript_easy.ts
+++ ts/typescript_easy.ts
@@ -6,78 +6,106 @@ window.addEventListener("load", function(): void {
     let easyButton: HTMLElement = document.getElementById("easyButton");
     let containerEasy: HTMLDivElement = document.querySelector(".containerEasy");
 
-    var players = ["x", "o"];
-    let currentPlayer: boolean = true;
 
-    let state: string = "";
-    let index: number = 0;
+    let currentPlayerIsPlayer0: boolean = true;
 
-    var board: number = 8; 
-    var  field: string = "";
+
+    var board: number = 8;
     let gameboard: HTMLDivElement;
-    let myArray: string[] = ["free", "free", "free", "free", "free", "free", "free", "free", "free"];
+
+    interface FieldType {symbol: string; }
+    let myArray: FieldType[] = [ {symbol: "free"}, {symbol: "free"}, {symbol: "free"}, {symbol: "free"}, {symbol: "free"}, {symbol: "free"}, {symbol: "free"}, {symbol: "free"}, {symbol: "free"} ];
 
 
 
     easyButton.addEventListener("click", drawField);
 
 
+    function drawField(): void {
 
-    easyButton.addEventListener("click", function(): void {
-        //alle Buttons verschwinden lassen
+        //alle Buttons verschwinden lassen und HTML einmal grundleeren
         levelButtons.innerHTML = "";
 
-    });
+        //nach jedem durchlauf Container leeren, sonst generieren sich immer neue 9 divs
+        containerEasy.innerHTML = "";
 
-
-    function drawField(event): void {
         //leichtes Spielfeld mit 9 divs erstellen
-       
-        for (index = 0; index <= board; index++) {
-            myArray[index];
+        for (let i: number = 0; i <= board; i++) {
             
             gameboard = document.createElement("div");
             gameboard.classList.add("field");
 
             let idField: Attr = document.createAttribute("id");
-            idField.value = index.toString();
+            idField.value = i.toString();
             
 
 
             containerEasy.appendChild(gameboard);
-            console.log(index);
-        }
+            gameboard.setAttributeNode(idField);
+            console.log(i);
+
+        
+        
 
         //Felder werden mit ensprechenden icons gefüllt
-        if (players[0]) {
-            const turn: HTMLElement = document.createElement("i");
-            turn.classList.add("fas fa-times");
-            console.log("Kreuz");
-        }
-        else {
-            const otherTurn: HTMLElement = document.createElement("i");
-            otherTurn.classList.add("far fa-circle");
-        }
+        //Spieler1 = x
+            if (myArray[i].symbol == "x") {
+                const turn: HTMLElement = document.createElement("i");
+                turn.classList.add("fas", "fa-times");
+                turn.style.color = "white";
+                turn.style.fontFamily = "cryon";
+                console.log("Kreuz");
+                
+
+                gameboard.appendChild(turn);
+            }
+
+        //Spieler2 = o
+             else if (myArray[i].symbol == "o") {
+                const otherTurn: HTMLElement = document.createElement("i");
+                otherTurn.classList.add("far", "fa-circle");
+                otherTurn.style.color = "white";
+                otherTurn.style.fontFamily = "cryon";
+                console.log("Kreis");
+
+
+                gameboard.appendChild(otherTurn);
+            }
 
 
         //überwachung der freien Felder, ob gedrückt wurde
-        if (myArray.state == "free") {
-            document.querySelector(".field").addEventListener("click", function(): void {clickFunction(); });
+            else if (myArray[i].symbol == "free") {
+                console.log("i is " + i);
+                gameboard.addEventListener("click", function(): void {clickFunction(i); });
+            }
+        
         }
-
-
+        
     }
 
 
  
 
 
-    function clickFunction(): void {
-        if (currentPlayer == true) {
-            
-        }
+    function clickFunction(positionImArray: number): void {
+        console.log("position is " + positionImArray);
+        for (let index: number  = 0; index <= board; index++) {
+            if (positionImArray == index) {
+                if (currentPlayerIsPlayer0 == true) {
+                    myArray[index].symbol = "x";
 
-        currentPlayer =! currentPlayer;
+                    console.log("Kreuz gedrückt");
+                 } 
+                 else {
+                    myArray[index].symbol = "o";
+
+                    console.log("Kreis gedrückt");
+                }
+
+             }
+        }
+        
+        currentPlayerIsPlayer0 = !currentPlayerIsPlayer0;
         drawField();
     }
 
@@ -94,8 +122,6 @@ window.addEventListener("load", function(): void {
         [2, 4, 6]
     ];
 
-
-    
 
 
 
